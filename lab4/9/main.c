46df8b6c02118b3ca21d2c2185cd12f706b2e10f
Ka1amboor <120525868+Ka1amboor@users.noreply.github.com>
2024-03-10 16:02:47 +0300

    Update main.c

--- lab4/9/main.c
+++ lab4/9/main.c
@@ -75,10 +75,7 @@ status_code read_information_from_file(
 
 
 // argv[1] - максимальный приоритет, argv[2] - файл с инструкциями, etc - заявки пользователей
-int main(
-        int argc,
-        char* argv[]
-        )
+int main(int argc,char* argv[])
 {
     if(argc < 3)
     {
@@ -98,31 +95,74 @@ int main(
     Structure_for_application heap;
     Second_structure structure;
 
-    char start_modeling[20], end_modeling[20];
+    char start_modeling[20];
     int min_time_by_operator, max_time_by_operator, count_departments;
     double overload_coef;
     Department* departments;
+    My_time* end_modeling = (My_time*)malloc(sizeof(My_time));
+    if(!end_modeling)
+    {
+        printf("failed to allocate memory for end modeling\n");
+        fclose(file_1);
+        return MEMORY_ERROR;
+    }
+
     //считали инф-ю из файла о структураъ и тд
     if(read_information_from_file(file_1, &heap, &structure) == INVALID_STRUCTURE)
     {
         printf("invalid structure in input file detected\n");
         fclose(file_1);
+        free(end_modeling);
         return INVALID_STRUCTURE;
     }
-
+    //считываем отсавшуюся информацию из файла
     fscanf(file_1, "%s", start_modeling);
-    fscanf(file_1, "%s", end_modeling);
+    fscanf(file_1, "%d-%d-%dT%d:%d:%d", &end_modeling->year, &end_modeling->month, &end_modeling->day, &end_modeling->hour, &end_modeling->minute, &end_modeling->second);
     fscanf(file_1, "%d", &min_time_by_operator);
     fscanf(file_1, "%d", &max_time_by_operator);
     fscanf(file_1, "%d", &count_departments);
 
+    if(count_departments  < 1 || count_departments > 100)
+    {
+        printf("detected invalid count of departments\n");
+        free(end_modeling);
+        fclose(file_1);
+        return INVALID_INPUT;
+    }
+
+
+    departments = (Department*)malloc(sizeof(Department));
+    if(!departments)
+    {
+        printf("failed to allocate memory for departments\n");
+        free(end_modeling);
+        fclose(file_1);
+        return MEMORY_ERROR;
+
+    }
+    //считываем из файла для каждого отделения количество операторов для него
+    for(int i = 0; i < count_departments; i++)
+    {
+        fscanf(file_1, "%d",&(departments[i].count_operators));
+        if(departments[i].count_operators < 10 || departments[i].count_operators > 50)
+        {
+            printf("detected invalid count of operators\n");
+            free(end_modeling);
+            free(departments);
+            fclose(file_1);
+            return INVALID_INPUT;
+        }
+        departments[i].count_real_applications = 0;
+        departments[i].count_free_applications = 0;
+    }
 
 
 
-    printf("%s\n%s\n%d\n%d\n%d\n", start_modeling, end_modeling, min_time_by_operator, max_time_by_operator, count_departments);
 
 
-
+    free(end_modeling);
+    free(departments);
+    fclose(file_1);
 
     return SUCCESS;
 }
